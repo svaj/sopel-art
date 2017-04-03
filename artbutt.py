9aff05cef269117319ded9b73c5547ef8af0213c
Chris Svajlenka <chris@svajlenka.com>
2017-04-03 14:34:17 -0400

    Adding some modifiers.

--- artbutt.py
+++ artbutt.py
@@ -12,6 +12,7 @@ no one in their right mind runs a PHP irc bot.
 """
 
 import datetime
+import math
 import random
 import threading  # This is so we can start flask in a thread. :D
 import sopel.module
@@ -75,6 +76,12 @@ class ArtSection(StaticSection):
     url = ValidatedAttribute('port', default="http://art.devhax.com/")
     """The art creation site's full URL."""
 
+    max_lines = ValidatedAttribute('max_lines', default=20)
+    """ The maximum number of lines an art can take up. """
+
+    max_cols = ValidatedAttribute('max_lines', default=30)
+    """ The maximum number of columns an art can take up. """
+
 
 art_schema = ArtSchema()
 
@@ -99,8 +106,9 @@ def art_after_get_many(result=None, search_params=None, **kw):
 
 
 def art_before_insert(data=None, **kw):
+    global local_bot
     """ This prepares new art before adding it to the db. """
-    data['irccode'] = convert_kinskode_to_irccode(data['kinskode'])
+    data['irccode'] = convert_kinskode_to_irccode(data['kinskode'], local_bot.config.art.max_lines, local_bot.config.art.max_cols)
     data['date'] = datetime.datetime.utcnow().isoformat()
 
 
@@ -203,7 +211,7 @@ def print_art(bot, current_art):
     bot.say("{} by {} (printed {} times now)".format(current_art.art, current_art.creator, current_art.display_count))
 
 
-def convert_kinskode_to_irccode(art_text):
+def convert_kinskode_to_irccode(art_text, max_lines=20, max_cols=30):
     """
     Converts "kinskode" art into IRC Codes (color codes, etc).
     :param art_text: The kinskode art.
@@ -232,13 +240,130 @@ def convert_kinskode_to_irccode(art_text):
     }
     prev_char = ''
     parsed = ''
-    for line in art_text.split('\n'):
+    for line in art_text.split('\n')[:max_lines]:
+        parsed_line = ''
         for char in line:
             char = char.upper()
             color = color_map[char]
             if prev_char == char:
-                parsed += fill
+                parsed_line += fill
             else:
-                parsed += "{}{},{}{}".format(c, str(color).zfill(2), str(color).zfill(2), fill)
-        parsed += "\n"
+                parsed_line += "{}{},{}{}".format(c, str(color).zfill(2), str(color).zfill(2), fill)
+        parsed += "{}\n".format(parsed_line[:max_cols])
+
     return parsed
+
+
+def apply_modifiers(kinskode='', modifiers=None):
+    mod_map = {
+        'i': 'invert',
+        'm': 'mirror',
+        'n': 'unitinu',
+        'd': 'divide',
+        'r': 'reverse',
+        'u': 'upsidedown',
+        's': 'square',
+        'f': 'flip',
+        'x': 'x'
+    }
+    unique_mods = set(modifiers)
+    if 'x' in unique_mods:
+        unique_mods = {'x'}
+    for mod in unique_mods:
+        if mod not in mod_map:
+            pass
+        else:
+            func = 'modify_' + mod_map[mod]
+            try:
+                kinskode = locals()[func](kinskode)
+            except Exception:
+                pass
+    return kinskode
+
+
+def strtr(strng, replace):
+    buffer = []
+    i, n = 0, len(strng)
+    while i < n:
+        match = False
+        for s, r in replace.items():
+            if strng[i:len(s)+i] == s:
+                buffer.append(r)
+                i = i + len(s)
+                match = True
+                break
+        if not match:
+            buffer.append(strng[i])
+            i = i + 1
+    return ''.join(buffer)
+
+
+def modify_invert(kinskode=''):
+    invert_map = {
+        ' ': 'A',
+        'A': ' ',
+        'B': 'H',
+        'C': 'M',
+        'D': 'J',
+        'E': 'K',
+        'F': 'I',
+        'G': 'L',
+        'H': 'B',
+        'I': 'M',
+        'J': 'D',
+        'K': 'E',
+        'L': 'G',  # H ?
+        'M': 'C',  # I ?
+        'N': 'O',
+        'O': 'N',
+        '_': ' '
+    }
+    return strtr(kinskode.upper(), invert_map)
+
+
+def modify_reverse(kinskode=''):
+    """ reverses kinskode. """
+    new_code = ''
+    for line in kinskode.split('\n'):
+        new_code += line[::-1] + '\n'  # reversed
+    return new_code
+
+
+def modify_upsidedown(kinskode=''):
+    return kinskode.split('\n')[::-1].join('\n')
+
+
+def modify_mirror(kinskode='', direction=0):
+
+    half = math.floor(max(kinskode.split('\n'), key=len))
+    if half < 1:
+        return kinskode
+    new_code = ''
+    for line in kinskode.split('\n'):
+        if direction == 0:
+            new_code += modify_reverse(line)[:half].trim() + line[half:] + '\n'
+        elif direction == 1:
+            new_code += line[:half] + modify_reverse(line)[half:].trim() + '\n'
+        else:
+            new_code += modify_reverse(line)[half:].trim() + line[:half] + '\n'
+    return new_code
+
+
+def modify_unitinu(kinskode=''):
+    return modify_mirror(kinskode, direction=1)
+
+
+def modify_divide(kinskode=''):
+    return modify_mirror(kinskode, direction=2)
+
+
+def modify_square(kinskode=''):
+    return kinskode
+
+
+def modify_flip(kinskode=''):
+    return kinskode
+
+
+def modify_x(kinskode=''):
+    return kinskode
